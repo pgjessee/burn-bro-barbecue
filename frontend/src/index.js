69d90d6850d0f597622a693635119c902b8b77c8
pgjessee <peter.jessee@gmail.com>
2021-01-07 08:13:31 -0600

    completed csrf and custom fetch route

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom'
+
 import './index.css';
+import { restoreCSRF, fetch } from './store/csrf';
 import App from './App';
+
+import configureStore from './store';
+
+
+const store = configureStore();
+
+if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+  window.csrfFetch = fetch;
+  window.store = store
+};
+
+function Root() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
   document.getElementById('root')
 );
