bd94addf76a5e6a2811909fff91aa22bb09ee581
pgjessee <peter.jessee@gmail.com>
2021-01-16 20:15:39 -0600

    added routes to the ingredients api to update and add to the food log

--- backend/routes/api/ingredients.js
+++ backend/routes/api/ingredients.js
@@ -1,18 +1,45 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
 
-const { Ingredient } = require('../../db/models')
+const { Ingredient, Food_Log } = require('../../db/models')
 
 const router = express.Router();
 
 
 router.get('/', asyncHandler(async(req, res) => {
-
     const ingredients = await Ingredient.findAll();
     const ingredientsJSon = res.json({ ingredients })
     return ingredientsJSon
 }));
 
+router.get('/:id(\\d+)', asyncHandler(async(req, res, next) => {
+    const ingredientId = parseInt(req.params.id, 10);
+    const ingredient = await Ingredient.findByPk(ingredientId)
+    return res.json({ ingredient });
+}))
 
+router.put('/:id(\\d+)', asyncHandler(async(req, res, next) => {
+    const { food_delta } = req.body;
+    const ingredientId = parseInt(req.params.id, 10);
+    const ingredient = await Ingredient.findByPk(ingredientId);
+
+    await ingredient.update({ food_in_stock: food_delta})
+    return res.json({ ingredient });
+}));
+
+
+router.post('/food-log', asyncHandler(async(req, res, next) => {
+    const { ingredient_id, employee_id, food_log_quantity, measurement_id} = req.body;
+
+    const foodLog = await Food_Log.create({
+        ingredient_id,
+        employee_id,
+        food_log_quantity,
+        measurement_id
+    })
+
+    return res.json({ foodLog });
+
+}))
 
 module.exports = router;
