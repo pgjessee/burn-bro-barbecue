a321f2fa59e1ad1913eb45a64e8751683cd33e82
pgjessee <peter.jessee@gmail.com>
2021-01-26 09:07:06 -0600

    Still debugging auth

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -31,7 +31,7 @@ router.post('/test', (req, res) => {
     res.json({ requestBody: req.body });
 });
 
-module.exports = router;
+
 
 router.get('/set-token-cookie', asyncHandler(async(req, res) => {
     const user = await User.findOne({
@@ -70,6 +70,9 @@ router.get(
 );
 
 
+router.post('/test', function(req, res) {
+    res.json({ requestBody: req.body });
+});
 
 
-
+module.exports = router;
