5eee22eadf7d457b4949007523054e8227aa2c3c
pgjessee <peter.jessee@gmail.com>
2021-02-13 11:47:28 -0600

    Added the demo login button on the splash page

--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import { NavLink, Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { NavLink} from 'react-router-dom';
 
+import * as sessionActions from '../../store/session';
 import './SplashPage.css'
 
 function SplashPage() {
+    const dispatch = useDispatch();
+
+    const handleDemoLogin = (e) => {
+        let credential = "demo@aa.io";
+        let password = "123";
+        e.preventDefault();
+        return dispatch(sessionActions.login({credential, password}))
+    };
 
     return (
         <div className="splash-navigation-container">
             <div>
                 <NavLink to="/menu"><button className="splash-navigation-button">View Our Menu</button></NavLink>
+            </div>
+            <div className="splash-button-container">
+                <button onClick={handleDemoLogin} className="splash-navigation-button">Login as Demo User</button>
             </div>
             <div className="splash-button-container">
                 <NavLink to="/login"><button className="splash-navigation-button">Log In</button></NavLink>
